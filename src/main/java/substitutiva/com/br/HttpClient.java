9ff30ac263e30cb7593f1e5374220a23389b8211
MrFrordoff <82807976+MrFrordoff@users.noreply.github.com>
2024-06-29 23:00:56 -0300

    HttpClient.java

--- src/main/java/substitutiva/com/br/HttpClient.java
+++ src/main/java/substitutiva/com/br/HttpClient.java
@@ -1,40 +1,26 @@
 package substitutiva.com.br;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
 public class HttpClient {
-    public static String getWeatherData(double latitude, double longitude) {
-        String urlString = String.format(
-            "https://api.open-meteo.com/v1/forecast?latitude=%.2f&longitude=%.2f&hourly=temperature_2m&start_date=2024-01-01&end_date=2024-01-31",
-            latitude, longitude
-        );
+    public static String makeHttpRequest(String urlString) throws Exception {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
 
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.connect();
-
-            int responseCode = conn.getResponseCode();
-            if (responseCode != 200) {
-                throw new RuntimeException("HttpResponseCode: " + responseCode);
-            } else {
-                StringBuilder inline = new StringBuilder();
-                Scanner scanner = new Scanner(url.openStream());
-
-                while (scanner.hasNext()) {
-                    inline.append(scanner.nextLine());
-                }
-
-                scanner.close();
-                return inline.toString();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
         }
+
+        in.close();
+        connection.disconnect();
+
+        return content.toString();
     }
 }
